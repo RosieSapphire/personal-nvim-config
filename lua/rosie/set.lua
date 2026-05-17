2747caa35f6b3f5072201fa913bc68d55f49f9cb
Rosie <jayjaygames40@gmail.com>
2026-05-17 15:51:47 -0700

    Updated the clang-format function so that it only updates the current buffer if something actually changed. That makes it easier to notice if we're already in good shapegit status

--- lua/rosie/set.lua
+++ lua/rosie/set.lua
@@ -581,6 +581,7 @@ vim.api.nvim_create_user_command('FormatFileWithClang', function()
         --        with formatting or not, causing less confusion.         --
         --------------------------------------------------------------------
 
+        -- Make sure we actually *have* clang-format, or we're fucked. lol
         if 0 == vim.fn.executable('clang-format') then
                 vim.notify("ERROR: `clang-format` not found on system! " ..
                            "Please install via your distro's package manager",
@@ -588,19 +589,36 @@ vim.api.nvim_create_user_command('FormatFileWithClang', function()
                 return
         end
 
+        -- Save the previous window position, 'cuz shit's gonna move around
         local view      = vim.fn.winsaveview()
+
+        -- Get all the lines in the file
         local lines     = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+        -- Perform the clang-format shenanigans on them
         local formatted = vim.fn.systemlist(
                 'clang-format --assume-filename=file.c --style=file --fallback-style=none 2>&1',
                 lines
         )
 
+        -- Make sure it actually ran successfully, otherwise print an error
         if 0 ~= vim.v.shell_error then
                 vim.notify(table.concat(formatted, vim.log.levels.ERROR))
                 return
         end
 
+        --[[
         vim.api.nvim_buf_set_lines(0, 0, -1, false, formatted)
+        ]]--
+        -- Make sure that we don't modify it if it hasn't changed.
+        -- This makes it much easier to know when we actually updated.
+        if vim.deep_equal(lines, formatted) then
+                vim.notify('No changes to file after formatting.', vim.log.levels.INFO)
+                return
+        end
+
+        -- Now restore our previous window view
+        -- so it looks like nothing happened!
         vim.fn.winrestview(view)
 end, {})
 
