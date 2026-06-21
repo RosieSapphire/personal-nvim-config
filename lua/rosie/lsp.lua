c5720f402b4b2648b260965df2bcd3cf11c6b7d0
Rosie <jayjaygames40@gmail.com>
2026-06-21 01:07:37 -0700

    lua/rosie/lsp.lua: Removed "CMake" that was commented out since I'm never gonna fucking use that retarded shit anyways, lmfao.

--- lua/rosie/lsp.lua
+++ lua/rosie/lsp.lua
@@ -60,17 +60,3 @@ vim.lsp.config('clangd', {
 })
 
 vim.lsp.enable('clangd')
-
--- CMake --
---[[
-vim.lsp.config('cmake', {
-	cmd = { 'cmake-language-server' },
-	filetypes = { 'cmake' },
-	root_markers = { 'CMakePresets.json', 'CTestConfig.cmake', '.git', 'build', 'cmake' },
-	init_options = {
-		buildDirectory = 'build',
-	},
-})
-
-vim.lsp.enable('cmake')
-]]-- This one doesn't work because I can't use pip (fucking trash) to install
