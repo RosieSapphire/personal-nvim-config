51d9d939713fbd05fe3441eaebd56d13879f22b7
Rosie <jayjaygames40@gmail.com>
2026-06-20 23:11:43 -0700

    lua/rosie/keymap/snippets/funcs.lua: Added functions for when I add the snippet files.

---
+++ lua/rosie/keymap/snippets/funcs.lua
@@ -0,0 +1,20 @@
+local dir = '$HOME/.config/nvim/snippets/'
+
+local M = {}
+
+function M.bind_create(keybind, path)
+        vim.keymap.set('n', keybind, function()
+                local file = vim.fn.expand(dir .. path)
+                vim.cmd("read " .. file)
+        end, { noremap = true, silent = true })
+end
+
+function M.bind_create_pragma(keybind, path)
+        vim.keymap.set('n', keybind, function()
+                local file = vim.fn.expand(dir .. path)
+                vim.cmd("read " .. file)
+                vim.api.nvim_feedkeys('j$i', 'n', false)
+        end, { noremap = true, silent = true })
+end
+
+return M
