c179e089b8b21258bf703639133e71ed761d25bf
Rosie <jayjaygames40@gmail.com>
2026-07-08 20:15:12 -0700

    lua/rosie/keymap/basic.lua: Reworked the debug information in "<leader>caec" such that I don't _need_ to turn it off! Additionally, I rewrote the FUCK out of "<leader>caeb"— insofar as it actually works now. lol

--- lua/rosie/keymap/basic.lua
+++ lua/rosie/keymap/basic.lua
@@ -195,84 +195,96 @@ vim.keymap.set('n', '<leader>bd', buf_cur_and_window_cur_del, {
         desc = "Save buf to existing file, close it AND it's window(s) too."
 })
 
-------------------------------------------------------------------------------
--- TODO: This is the first function I wrote that has actual fucken' details --
---       about what it's doing every step of the process.                   --
---                                                                          --
---       This is really great, but I want to be able to turn on and off the --
---       debug information like how I would in C to make it controllabel!   --
-------------------------------------------------------------------------------
 vim.keymap.set('n', '<leader>caec', function()
-        --[[
-        vim.notify(
-                "[CLOSE ALL EXCEPT CURRENT] <leader>caec:",
-                vim.log.levels.INFO
-        )
-        ]]--
+         vim.notify(
+                 "[CLOSE ALL EXCEPT CURRENT] <leader>caec:",
+                 vim.log.levels.INFO
+         )
 
         --------------------------------------------------------
         -- Create a set of "protected" buffers that don't get --
         -- deleted when the rest of them get fucken' nuked.   --
         --------------------------------------------------------
         local bufs_keep = get_bufs_used_in_open_windows()
-        -- local left      = {}
+        local nuked     = {}
 
-        -- vim.notify("\tDeleting all buffers not currently used by window.")
+        vim.notify(
+                "\tNuking all buffers not currently used by an open window.",
+                 vim.log.levels.INFO
+        )
+
         for _, buf in ipairs(vim.api.nvim_list_bufs()) do
                 local buf_is_normal = vim.api.nvim_buf_is_valid(buf)
                                   and vim.bo[buf].buflisted
                                   and vim.bo[buf].buftype == ''
 
                 -- Only delete VISIBLE, NORMAL buffers that are NOT protected.
+
+                --------------------------------------------------------------
+                -- FIXME: Is this fucken indentation _really_ necessary?    --
+                --------------------------------------------------------------
                 if buf_is_normal then
                         if not bufs_keep[buf] then
-                                --[[
-                                vim.notify(
-                                        "\t\t" .. buf ..
-                                        " [ name = { \"" ..
-                                        vim.api.nvim_buf_get_name(buf) ..
-                                        "\" } ]",
-                                        vim.log.levels.INFO
-                                );
-                                ]]--
+                                ----------------------------------------------
+                                -- TODO: Add ability to tell which files    --
+                                --       were actually saved or not.        --
+                                ----------------------------------------------
                                 if buf_save_if_existing_file(buf) then
+                                        table.insert(nuked, buf)
                                         buf_del_force(buf)
+                                        vim.notify(
+                                                "\t\tRemoved buffer " ..
+                                                buf .. ".",
+                                                vim.log.levels.INFO
+                                        );
                                 end
-                        --[[
-                        else
-                                table.insert(left, buf)
-                        ]]--
                         end
                 end
         end
 
-        --[[
-        vim.notify(
-                "\tWe were left with " .. #left .. " buffers after cleanup.",
-                vim.log.levels.INFO
-        );
-        for _, buf in ipairs(left) do
+        if #nuked ~= 0 then
                 vim.notify(
-                        "\t\t" .. buf ..
-                        " [ name = { \"" .. vim.api.nvim_buf_get_name(buf) ..
-                        "\" } ]",
+                        "Removed " .. #nuked .. " unfocused buffers.",
+                        vim.log.levels.INFO
+                );
+        else
+                vim.notify(
+                        "No unfocused buffers to remove.",
                         vim.log.levels.INFO
                 );
         end
-        ]]--
 end, { desc = "Closes all buffers except visible ones.", silent = true })
 
 vim.keymap.set('n', '<leader>caeb', function()
-        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-                local buf_is_normal =
-                        vim.api.nvim_buf_is_valid(buf)
-                    and vim.bo[buf].buflisted
-                    and vim.bo[buf].buftype == ''
+        vim.notify("[CLOSE ALL EMPTY BUFFERS] <leader>caeb:", vim.log.levels.INFO)
 
-                -- Only delete NORMAL buffers that are NOT protected.
-                if buf_is_normal and not bufs_keep[buf] then
-                        if buf_save_if_existing_file(buf) then
-                                buf_del_force(buf)
+        local empty = {}
+
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+                local is_valid = vim.api.nvim_buf_is_valid(buf) and
+                                 vim.bo[buf].buftype == '' and
+                                 vim.bo[buf].buflisted
+
+                if is_valid then
+                        local name  = vim.api.nvim_buf_get_name(buf)
+
+                        -- Only append buffers with no names.
+                        if name == '' then
+                                vim.notify(
+                                        "\t\tBuffer " .. buf .. " is empty.",
+                                        vim.log.levels.INFO
+                                )
+                                table.insert(empty, buf)
                         end
                 end
         end
+
+        for _, buf in ipairs(empty) do
+                buf_del_force(buf)
+        end
+
+        vim.notify(
+                "Removed " .. #empty .. " empty buffers!",
+                vim.log.levels.INFO
+        )
+end, { desc = "Closes all empty buffers, visible or not", silent = true })
